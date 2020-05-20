f674eb6dfc6e57317f8af3b8e99b69bffde05205
Boueya Hethsron <hethsronboueya@gmail.com>
2020-05-20 20:08:36 +0200

    update view.hpp and core.hpp

--- imt/include/core/core.hpp
+++ imt/include/core/core.hpp
@@ -1,4 +1,6 @@
 #ifndef CORE_HPP
 #define CORE_HPP
 
+#include <core/boxes.hpp>
+
 #endif // ! CORE_HPP
--- imt/include/view/view.hpp
+++ imt/include/view/view.hpp
@@ -1,4 +1,7 @@
 #ifndef VIEW_HPP
 #define VIEW_HPP
 
+#include <view/mainview.hpp>
+#include <view/splashview.hpp>
+
 #endif // ! VIEW_HPP
