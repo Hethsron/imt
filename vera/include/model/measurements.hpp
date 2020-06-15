9afca8af55bc6134d8c808a3d31d6f31d641afee
Boueya Hethsron <hethsronboueya@gmail.com>
2020-06-15 22:12:36 +0200

    adding measurements for Wearables

--- vera/include/model/measurements.hpp
+++ vera/include/model/measurements.hpp
@@ -24,4 +24,203 @@
  *  \date 			21th May 2020
  */
 
+#include <QtCore/QtCore>
+#include <iostream>
+
+class Measurements
+{
+    public:
+        /**
+         * @fn      Measurements
+         * @brief   Default constructor of class
+         */
+        Measurements();
+
+        /**
+         * @fn      Measurements
+         * @brief   Parameterized constructor of class
+         * 
+         * @param[in]       key             Measurements name
+         */
+        Measurements(const QString& key);
+
+        /**
+         * @fn      Measurements
+         * @brief   Copy constructor of class
+         * 
+         * @param[in]       other           Measurements instance
+         */
+        Measurements(const Measurements& other);
+
+        /**
+         * @fn      Measurements
+         * @brief   Copy constructor of class
+         * 
+         * @param[in]       ptr             Null instance
+         */
+        Measurements(std::nullptr_t ptr);
+
+        /**
+         * @fn      ~Measurements
+         * @brief   Destructor of class
+         */
+        virtual ~Measurements();
+
+        /**
+         * @fn      getName
+         * @brief   Virtual function that returns the measurement name
+         * 
+         * @return          Measurement name
+         */
+        virtual QString getName() const;
+
+        /**
+         * @fn      getTime
+         * @brief   Virtual function that returns the measurements time data
+         * 
+         * @return          Measurements time data
+         */
+        virtual QVector<double> getTime() const;
+
+        /**
+         * @fn      getAccelX
+         * @brief   Virtual function that returns the accelerometer X data
+         * 
+         * @return          Accelerometer X data
+         */
+        virtual QVector<double> getAccelX() const;
+
+        /**
+         * @fn      getAccelY
+         * @brief   Virtual function that returns the accelerometer Y data
+         * 
+         * @return          Accelerometer Y data
+         */
+        virtual QVector<double> getAccelY() const;
+
+        /**
+         * @fn      getAccelZ
+         * @brief   Virtual function that returns the accelerometer Z data
+         * 
+         * @return          Accelerometer Z data
+         */
+        virtual QVector<double> getAccelZ() const;
+
+        /**
+         * @fn      getGyroX
+         * @brief   Virtual function that returns the gyroscope X data
+         * 
+         * @return          Gyroscope X data
+         */
+        virtual QVector<double> getGyroX() const;
+
+        /**
+         * @fn      getGyroY
+         * @brief   Virtual function that returns the gyroscope Y data
+         * 
+         * @return          Gyroscope Y data
+         */
+        virtual QVector<double> getGyroY() const;
+
+        /**
+         * @fn      getGyroZ
+         * @brief   Virtual function that returns the gyroscope Z data
+         * 
+         * @return          Gyroscope Z data
+         */
+        virtual QVector<double> getGyroZ() const;
+
+        /**
+         * @fn      getMaxAccel
+         * @brief   Virtual function that returns the maximum value of the plot axis between accelX, accelY and accelZ
+         * 
+         * @return          Maximum value of the plot axis between accelX, accelY and accelZ
+         */
+        virtual double getMaxAccel() const;
+
+        /**
+         * @fn      getMinAccel
+         * @brief   Virtual function that returns the minimum value of the plot axis between accelX, accelY and accelZ
+         * 
+         * @return          Minimum value of the plot axis between accelX, accelY and accelZ
+         */
+        virtual double getMinAccel() const;
+
+        /**
+         * @fn      getMaxGyro
+         * @brief   Virtual function that returns the maximum value of the plot axis between gyroX, gyroY and gyroZ
+         * 
+         * @return          Maximum value of the plot axis between gyroX, gyroY and gyroZ
+         */
+        virtual double getMaxGyro() const;
+
+        /**
+         * @fn      getMinGyro
+         * @brief   Virtual function that returns the minimum value of the plot axis between gyroX, gyroY and gyroZ
+         * 
+         * @return          Minimum value of the plot axis between gyroX, gyroY and gyroZ
+         */
+        virtual double getMinGyro() const;
+
+        /**
+         * @fn      isEmpty
+         * @brief   Virtual function that returns true if time, accelerometer and gyroscope data are empty, false otherwise
+         * 
+         * @return          true if time, accelerometer and gyroscope data are empty, false otherwise
+         */
+        virtual bool isEmpty() const;
+
+        /**
+         * @fn      clear
+         * @brief   Virtual function that clears all time, accelerometer and gyroscope data
+         */
+        virtual void clear();
+
+        /**
+         * @fn      append
+         * @brief   Virtual function that appends time, accelerometer and gyroscope data from given JSON object
+         * 
+         * @param[in]       ms              Current time
+         * @param[in]       obj             Given JSON object
+         */
+        virtual void append(double ms, const QJsonObject& obj);
+
+        /**
+         * @fn      operator=
+         * @brief   Assignment operator overloading
+         * 
+         * @param[in]       other           Measurement instance
+         * @return          Right Measurement instance
+         */
+        Measurements& operator=(const Measurements& other);
+
+        /**
+         * @fn      operator==
+         * @brief   Equality operator overloading
+         * 
+         * @param[in]       other           Measurement instance
+         * @return          true if measurement seems to be the same, false otherwise
+         */
+        bool operator==(const Measurements& other) const;
+
+        /**
+         * @fn      operator!=
+         * @brief   Inequality operator overloading
+         * 
+         * @param[in]       other           Measurement instance
+         * @return          true if measurement doesn't seem to be the same, false otherwise
+         */
+        bool operator!=(const Measurements& other) const;
+    
+    private:
+        QString name;                       /*!< Measurement name */
+        QVector<double> time;               /*!< Measurement time */
+        QVector<double> accelX;             /*!< Accelerometer X data */
+        QVector<double> accelY;             /*!< Accelerometer Y data */
+        QVector<double> accelZ;             /*!< Accelerometer Z data */
+        QVector<double> gyroX;              /*!< Gyroscope X data */
+        QVector<double> gyroY;              /*!< Gyroscope Y data */
+        QVector<double> gyroZ;              /*!< Gyroscope Z data */
+};
+
 #endif // ! MEASUREMENTS_HPP
--- vera/src/model/measurements.cpp
+++ vera/src/model/measurements.cpp
@@ -1,1 +1,201 @@
 #include <model/measurements.hpp>
+
+Measurements::Measurements()
+    : name(QString()) {}
+
+Measurements::Measurements(const QString& key)
+    : name(key), time(QVector<double>()), accelX(QVector<double>()), accelY(QVector<double>()), accelZ(QVector<double>()), gyroX(QVector<double>()), gyroY(QVector<double>()), gyroZ(QVector<double>()) {}
+
+Measurements::Measurements(const Measurements& other)
+    :name(other.name), time(other.time), accelX(other.accelX), accelY(other.accelY), accelZ(other.accelZ), gyroX(other.gyroX), gyroY(other.gyroY), gyroZ(other.gyroZ) {}
+
+Measurements::Measurements(std::nullptr_t)
+    : name(QString()), time(QVector<double>()), accelX(QVector<double>()), accelY(QVector<double>()), accelZ(QVector<double>()), gyroX(QVector<double>()), gyroY(QVector<double>()), gyroZ(QVector<double>()) {}
+
+Measurements::~Measurements() {
+    // Check if data is not empty
+    if (!isEmpty()) {
+        // Clear all data
+        clear();
+    }
+}
+
+QString Measurements::getName() const {
+    return name;
+}
+
+QVector<double> Measurements::getTime() const {
+    return time;
+}
+
+QVector<double> Measurements::getAccelX() const {
+    return accelX;
+}
+
+QVector<double> Measurements::getAccelY() const {
+    return accelY;
+}
+
+QVector<double> Measurements::getAccelZ() const {
+    return accelZ;
+}
+
+QVector<double> Measurements::getGyroX() const {
+    return gyroX;
+}
+
+QVector<double> Measurements::getGyroY() const {
+    return gyroY;
+}
+
+QVector<double> Measurements::getGyroZ() const {
+    return gyroZ;
+}
+
+double Measurements::getMaxAccel() const {
+    double value = accelX.at(0);
+
+    for (int i = 0; i < accelX.size(); ++i) {
+        if (accelX.at(i) > value) {
+            value = accelX.at(i);
+        }
+    }
+
+    for (int i = 0; i < accelY.size(); ++i) {
+        if (accelY.at(i) > value) {
+            value = accelY.at(i);
+        }
+    }
+
+    for (int i = 0; i < accelZ.size(); ++i) {
+        if (accelZ.at(i) > value) {
+            value = accelZ.at(i);
+        }
+    }
+
+    return value;
+}
+
+double Measurements::getMinAccel() const {
+    double value = accelX.at(0);
+    
+    for (int i = 0; i < accelX.size(); ++i) {
+        if (accelX.at(i) < value) {
+            value = accelX.at(i);
+        }
+    }
+
+    for (int i = 0; i < accelY.size(); ++i) {
+        if (accelY.at(i) < value) {
+            value = accelY.at(i);
+        }
+    }
+
+    for (int i = 0; i < accelZ.size(); ++i) {
+        if (accelZ.at(i) < value) {
+            value = accelZ.at(i);
+        }
+    }
+
+    return value;
+}
+
+double Measurements::getMaxGyro() const {
+    double value = gyroX.at(0);
+
+    for (int i = 0; i < gyroX.size(); ++i) {
+        if (gyroX.at(i) > value) {
+            value = gyroX.at(i);
+        }
+    }
+
+    for (int i = 0; i < gyroY.size(); ++i) {
+        if (gyroY.at(i) > value) {
+            value = gyroY.at(i);
+        }
+    }
+
+    for (int i = 0; i < gyroZ.size(); ++i) {
+        if (gyroZ.at(i) > value) {
+            value = gyroZ.at(i);
+        }
+    }
+
+    return value;
+}
+
+double Measurements::getMinGyro() const {
+    double value = gyroX.at(0);
+
+    for (int i = 0; i < gyroX.size(); ++i) {
+        if (gyroX.at(i) < value) {
+            value = gyroX.at(i);
+        }
+    }
+
+    for (int i = 0; i < gyroY.size(); ++i) {
+        if (gyroY.at(i) < value) {
+            value = gyroY.at(i);
+        }
+    }
+
+    for (int i = 0; i < gyroZ.size(); ++i) {
+        if (gyroZ.at(i) < value) {
+            value = gyroZ.at(i);
+        }
+    }
+
+    return value;
+}
+
+bool Measurements::isEmpty() const {
+    return (time.size() == 0 && accelX.size() == 0 && accelY.size() == 0 && accelZ.size() == 0 && gyroX.size() == 0 && gyroY.size() == 0 && gyroZ.size() == 0);
+}
+
+void Measurements::clear() {
+    // Clear time data
+    time.clear();
+
+    // Clear accelorometer data
+    accelX.clear();
+    accelY.clear();
+    accelZ.clear();
+
+    // Clear gyroscope data
+    gyroX.clear();
+    gyroY.clear();
+    gyroZ.clear();
+}
+
+void Measurements::append(double ms, const QJsonObject& obj) {
+    // Appends time data
+    time.append(ms);
+
+    // Appends accelerometer data
+    accelX.append(obj.value(QString("accel_x")).toDouble());
+    accelY.append(obj.value(QString("accel_y")).toDouble());
+    accelZ.append(obj.value(QString("accel_z")).toDouble());
+
+    // Appends gyroscope data
+    gyroX.append(obj.value(QString("gyro_x")).toDouble());
+    gyroY.append(obj.value(QString("gyro_y")).toDouble());
+    gyroZ.append(obj.value(QString("gyro_z")).toDouble());
+}
+
+Measurements& Measurements::operator=(const Measurements& other) {
+    // Avoid self assignment
+    if (this != &other) {
+        if (other.name != name) {
+            name = other.name;
+        }
+    }
+    return *this;
+}
+
+bool Measurements::operator==(const Measurements& other) const {
+    return (other.name == name);
+}
+
+bool Measurements::operator!=(const Measurements& other) const {
+    return (other.name != name);
+}
