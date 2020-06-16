c37e38ada14bbc47ec9f64086ab4aa1eb482e124
Boueya Hethsron <hethsronboueya@gmail.com>
2020-06-16 13:00:43 +0200

    update branch

--- vera/src/model/wearables.cpp
+++ vera/src/model/wearables.cpp
@@ -61,7 +61,59 @@ void Wearables::append(const QString& fileName) {
     if (file.exists()) {
         // Open file
         if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-            
+            // Define line
+            QString line;
+
+            // Define status
+            bool status = true;
+
+            // Define times
+            double firstTime = 0;
+            double time;
+
+            // Define json document
+            QJsonDocument config;
+
+            // Define text stream
+            QTextStream in(&file);
+            while (!in.atEnd()) {
+                // Getting line
+                line = in.readLine();
+
+                // Getting json document
+                config = QJsonDocument::fromJson(line.toUtf8());
+
+                // Getting json object
+                QJsonObject obj = config.object();
+
+                // Check if it is first time
+                if (status) {
+                    firstTime = obj.value(QString("time")).toDouble();
+                    status = false;
+                }
+
+                // Getting name of wearable
+                const QString name = obj.value(QString("name")).toString();
+
+                // Getting time
+                time = obj.value(QString("time")).toDouble() - firstTime;
+
+                // Check if given name exists in measurements
+                if (contains(name)) {
+                    // Appends time, accelerometer and gyroscope data
+                    get(name)->append(time, obj);
+                }
+                else {
+                    // Append new measurement data
+                    measurements.append(new Measurements(name));
+
+                    // Appends time, accelerometer and gyroscope data
+                    get(name)->append(time, obj);
+                }
+            }
+
+            // Close file
+            file.close();
         }
     }
 }
